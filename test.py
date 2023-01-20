de37ef483af609d8a42263f6302551eff89c7aa4
marcin <gawryszczakmarcin@gmail.com>
2023-01-20 16:38:43 +0100

    rozwiazane zadanko

---
+++ test.py
@@ -0,0 +1,54 @@
+import requests
+from requests.exceptions import HTTPError
+import json
+
+class Cinkciarz:
+    def __init__(self):
+        print('Cinciarz stworzony')
+
+    def przelicz_na_PLN(self, kod, ile):
+        try:
+    # dokumentację API dla NBP znajdziesz pod adresem http://api.nbp.pl/
+            url = f'http://api.nbp.pl/api/' \
+                f'exchangerates/rates/a/' \
+                f'{kod}/' \
+                f'?format=json'
+            response = requests.get(url)
+        except HTTPError as http_error:
+            print(f'HTTP error: {http_error}')
+        except Exception as e:
+            print(f'Other exception: {e}')
+        else:
+            if response.status_code == 200:
+                response = response.json()
+                kurs = response['rates'][0]['mid']
+        total_PLN = ile*kurs
+        print(total_PLN, 'zł')
+        
+    def zapytaj_o_kurs(self, kod):
+        try:
+    # dokumentację API dla NBP znajdziesz pod adresem http://api.nbp.pl/
+            url = f'http://api.nbp.pl/api/' \
+                f'exchangerates/rates/a/' \
+                f'{kod}/' \
+                f'?format=json'
+            response = requests.get(url)
+        except HTTPError as http_error:
+            print(f'HTTP error: {http_error}')
+        except Exception as e:
+            print(f'Other exception: {e}')
+        else:
+            if response.status_code == 200:
+                response = response.json()
+                kurs = response['rates'][0]['mid']
+                print(kurs, 'zł')
+    
+# przykładowe zastosowanie
+# stworzenie klasy
+cinkciarz = Cinkciarz() 
+
+#prośba o kurs
+kurs = cinkciarz.zapytaj_o_kurs('chf')
+
+#prośba o przeliczenie
+przeliczenie = cinkciarz.przelicz_na_PLN('eur', 6000)
